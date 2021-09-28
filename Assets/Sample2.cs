c184a01bbb64726e9018bdca6fb841e501c0e064
TVPhu <phutvfx01501@funix.edu.vn>
2021-09-28 09:03:47 +0700

    fix auto complete of Visual Studio

--- Assets/Sample2.cs
+++ Assets/Sample2.cs
@@ -7,7 +7,11 @@ public class Sample2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(transform.position.x);
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
     }
 
     // Update is called once per frame
